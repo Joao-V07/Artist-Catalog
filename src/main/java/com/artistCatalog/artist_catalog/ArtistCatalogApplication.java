75bf61cb06fd697223cd58ffd582b672bad23e62
Joao-V07 <Joaovitorodasilva@hotmail.com>
2026-09-15 18:38:43 +1000

    Initial Commit.

---
+++ src/main/java/com/artistCatalog/artist_catalog/ArtistCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.artistCatalog.artist_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artistCatalog/artist_catalog/ArtistCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistCatalog.artist_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
