b5c817a9601151b85e8aa685f0ea9e250645d8f5
Joao-V07 <Joaovitorodasilva@hotmail.com>
2026-09-25 16:56:11 +1000

    Implement base MusicService class
    Implemented a generic

--- src/main/java/com/artistCatalog/artist_catalog/Services/MusicService.java
+++ src/main/java/com/artistCatalog/artist_catalog/Services/MusicService.java
@@ -1,4 +1,37 @@
 package com.artistCatalog.artist_catalog.Services;
 
+import com.artistCatalog.artist_catalog.Models.Music;
+import com.artistCatalog.artist_catalog.Repositories.MusicRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MusicService {
+    private final MusicRepository repository;
+
+    public MusicService(MusicRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Music> findAll(){
+        return repository.findAll();
+    }
+
+    public void deleteById(long id) {
+        repository.deleteById(id);
+    }
+
+    public void save(Music music) {
+        repository.save(music);
+    }
+
+    public void update(Long id, Music musicDetails) {
+        Music music = repository.findById(id).orElseThrow(() -> new RuntimeException("Music not found"));
+
+        music.setName(musicDetails.getName());
+        music.setReleaseYear(musicDetails.getReleaseYear());
+        music.setAlbum(musicDetails.getAlbum());
+    }
 }
